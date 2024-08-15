ed2bcc7e578e28d5ce66d989943d98490ff7c2cb
esra <esrabaskaya84@gmail.com>
2024-08-15 11:36:29 +0200

    firebase added

---
+++ src/hooks/useAuthCalls.jsx
@@ -0,0 +1,12 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const useAuthCalls = () => {
+	const createUser = async (email, password) => {
+		try {
+			createUserWithEmailAndPassword(auth, email, password);
+		} catch (error) {}
+	};
+	return { createUser };
+};
+
+export default useAuthCalls;
