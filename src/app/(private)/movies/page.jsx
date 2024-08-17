8a12c3231d80703febc8d1b5e14d6c8ea33aeea7
esra <esrabaskaya84@gmail.com>
2024-08-17 22:07:58 +0200

    "Updated .env file, HeroSection and Movies components, globals.css, and MovieFunctions.js"

--- src/app/(private)/movies/page.jsx
+++ src/app/(private)/movies/page.jsx
@@ -1,10 +1,17 @@
+import { getMovies } from "@/helpers/MovieFunctions";
 import React from "react";
 import HeroSection from "./components/HeroSection";
 
-const Movies = () => {
+const Movies = async () => {
+	const movies = await getMovies("now_playing");
+	// console.log(movies);
 	return (
 		<div>
-			<HeroSection />
+			<HeroSection
+				title={movies[0].title}
+				overview={movies[0].overview}
+				id={movies[0].id}
+			/>
 		</div>
 	);
 };
